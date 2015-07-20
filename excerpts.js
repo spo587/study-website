4ee32a108022e9032e8388f668dd30263ef0d164
spo587 <samueljpost@gmail.com>
2015-07-20 18:15:23 -0400

    first commit

---
+++ excerpts.js
@@ -0,0 +1,37 @@
+var excerpts = {
+                    beethovenLinks:
+
+                        {name: 'beethoven',
+                        video: {
+                            long: 
+                                {links:
+                                    ['https://www.youtube.com/embed/npNtdbF5k3k', 
+                                    'https://www.youtube.com/embed/WigDFSmOcOw', 
+                                    'https://www.youtube.com/embed/n8ndHYBEPsw']
+                                },
+                            short:
+                                {links:
+                                    ['https://www.youtube.com/embed/Ah392lnFHxM', 'test2', 'test3']}
+                            },
+                        audio: {
+                            long:
+                                {links:
+                                    ['http://samueljpost.com/uploads/1/2/5/1/125199/musicpostep14_apple_tree_1.mp3', 
+                                    'test2', 
+                                    'test3']
+                                },
+                            short:
+                                {links:
+                                    []
+                                }
+                            
+                        }
+                    },
+                    bachLinks:
+                        {}
+                };
+
+
+//exports.beethovenLinks = beethovenLinks;
+
+module.exports = excerpts;
